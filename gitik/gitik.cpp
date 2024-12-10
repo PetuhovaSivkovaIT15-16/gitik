da05d3bb7ab122d1853edeece80b11b4840a6068
Екатерина Сивкова <katasivkova352@gmail.com>
2024-12-10 18:00:40 +0500

    Добавлена функция для перемешивания чисел на игровом поле

--- gitik/gitik.cpp
+++ gitik/gitik.cpp
@@ -28,10 +28,51 @@ void zapolnenie(int pole[n][n], int& pstr, int& pstl)
 	}
 
 }
+//перемешивание чисел на игровом поле
+void peremeshivanie(int pole[n][n], int& pstr, int& pstl)
+{
+	int mas[n * n - 1];
+	int index = 0;
+	for (int i = 0;i < n;i++)
+	{
+		for (int j = 0;j < n;j++)
+		{
+			if (i* n + j < n * n - 1)
+				mas[index++] = pole[i][j];
+		}
+	}
+	srand(time(0));
+	for (int i = n * n - 2;i > 0;i--)
+	{
+		int j = rand() % (i + 1);
+		if (j != i)
+		{
+			int dop = mas[i];
+			mas[i] = mas[j];
+			mas[j] = dop;
+		}
+	}
+	index = 0;
+	for (int i = 0;i < n;i++)
+	{
+		for (int j = 0; j < n;j++)
+		{
+			if (i * n + j < n * n - 1)
+				pole[i][j] = mas[index++];
+			else
+			{
+				pole[i][j] = 0;
+				pstr = i;
+				pstl = j;
+			}
+		}
+	}
+}
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	int pole[n][n];
 	int pstr, pstl;
 	zapolnenie(pole, pstr, pstl);
+	peremeshivanie(pole, pstr, pstl);
 }
